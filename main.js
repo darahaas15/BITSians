7b4f2db54f3ed55792c33eeee944d81ddb55b15b
robo-mop <realaryanpingle@gmail.com>
2021-10-30 21:38:38 +0530

    Posterity

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ const sort_multiple = (arr, key=e=>e)=>{arr.sort((a,b)=>{
 const max = (iterable, key=e=>e)=>iterable.reduce((acc,val)=>key(acc)>key(val)?acc:val, iterable[0])
 const min = (iterable, key=e=>e)=>iterable.reduce((acc,val)=>key(acc)<key(val)?acc:val, iterable[0])
 const gid = id=>document.getElementById(id)
+const logtime = (stime, process, color="greenyellow")=>print(`%c${process}%c completed in %c${new Date()-stime}ms%c`, "background-color: white; color: black; font-weight: 700;", "", `color: ${color};`, "")
+
+var filtered = []
+const MAX_RESULT_COUNT = 200
 
 window.onload = ()=>{
     // Setup toggles
@@ -24,7 +28,9 @@ window.onload = ()=>{
     // Setup 'Select all' buttons
     setup_select_all()
 
+    // Store the input element in query_input
     query_input = gid("query")
+    apply_filters()
     // Initial call to show results
     resolve_query()
     // Set key event for query input
@@ -36,6 +42,7 @@ function setup_toggles() {
         toggle.onclick = event=>{
             if(toggle.classList.contains("selected")) toggle.classList.remove("selected")
             else toggle.classList.add("selected")
+            apply_filters()
         }
     })
 }
@@ -52,6 +59,7 @@ function setup_select_all() {
                 // Select all
                 [...container.children].forEach(child=>child.classList.add("selected"))
             }
+            apply_filters()
         }
     })
 }
@@ -80,14 +88,16 @@ function load_filters() {
 function apply_filters() {
     let stime = new Date()
     load_filters()
-    let output = everyone.filter(person=>{
+    filtered = everyone.filter(person=>{
         let matches = filters.filter(([category, queries])=>{
             return queries.includes(person[category])
         }).length
         return matches == filters.length
     })
-    print(`Query completed in ${new Date()-stime}ms`)
-    return output
+    logtime(stime, "apply_filters")
+    if(window.innerWidth >= 985) {
+        resolve_query()
+    }
 }
 
 function close_filters_tab() {
@@ -99,47 +109,64 @@ function show_filters_tab() {
 }
 
 function resolve_query() {
+    let stime = new Date()
     let query = query_input.value.replace(/\s+/g, ' ')
-    results = Object.entries(everyone).map(person=>{
-        let [room, [name, branch, phone]] = person
-        let score = scorer(query, [name, room, branch, phone][CRITERION])
-        return [score, room, name, branch, phone]
-    })
+    let results = []
+    if(/\d/.test(query)) {
+        print("Testing against IDs")
+        // Test against ids
+        results = filtered.map(person=>{
+            let score = scorer_id(query, person["ID"])
+            return [score, person]
+        })
+    }
+    else {
+        print("Testing against names")
+        results = filtered.map(person=>{
+            let score = scorer_name(query, person["name"])
+            return [score, person]
+        })
+    }
     sort_multiple(results, element=>[-element[0][0], -element[0][1], element[0][2]])
-    results = results.filter(element=>element[0][0])
-    gid("results").innerHTML = results.map(ele=>`<div class="student">
-    <div class="student-room">
-    ${ele[1]}
-    </div>
-    <div class="student-info">
-    <div class="student-name">${ele[2].replace(/(?<=.{9,})\s+/g, "<br>").replace(/\s+(?=[a-zA-Z\.]{9,})/g, "<br>")}</div> 
-    <div class="student-branch">${ele[3].replace(" + ", "<br>")}</div>
-    <div class="student-phone">+91 ${ele[4]}</div>
-    </div>
-    </div>
-    `).join("")
-    
-    enable_copier()
+    results = results.filter(element=>element[0][0]).slice(0, MAX_RESULT_COUNT)
+    results = results.map(([score, person])=>person)
+    logtime(stime, "resolve_query")
+    print(results)
+
+    display_results(results)
 }
 
-function enable_copier() {
-    [...document.getElementsByClassName("student")].forEach(ele=>{
-        ele.onclick = event=>window.open("tel:"+ele.getElementsByClassName("student-phone")[0].innerHTML)
-    })
+function display_results(results) {
+    // gid("results").innerHTML = results.map(ele=>`<div class="student">
+    // <div class="student-room">
+    // ${ele[1]}
+    // </div>
+    // <div class="student-info">
+    // <div class="student-name">${ele[2].replace(/(?<=.{9,})\s+/g, "<br>").replace(/\s+(?=[a-zA-Z\.]{9,})/g, "<br>")}</div> 
+    // <div class="student-branch">${ele[3].replace(" + ", "<br>")}</div>
+    // <div class="student-phone">+91 ${ele[4]}</div>
+    // </div>
+    // </div>
+    // `).join("")
 }
 
-function scorer(query, text) {
+/**
+ * Matches a query to the given text and returns its score
+ * @returns List of 3 scores - [number of character matches, longest common substring, start index of LCS]
+ */
+
+function scorer_name(query, text) {
     if(query.length == 0) {
         return [1, 0, 0]
     }
     text = text.toLowerCase()
     query = query.toLowerCase()
     let j = 0
-    let max_consec = [0,0]
-    let consec = 0
+    let max_consec = [0, 0]
+    let consec = [0, 0]
     for(let i = 0; i < text.length; ++i) {
         if(text.charAt(i) == query.charAt(j)) {
-            consec = [1,i]
+            consec = [1, i]
             // Loop behind to see how consecutive this is
             let k = i-1
             while(k >= 0 && text.charAt(k) == query.charAt(j - (i-k))) {
@@ -160,4 +187,11 @@ function scorer(query, text) {
         return [j, ...max_consec]
     }
     else return [0, 0, 0]
+}
+
+function scorer_id(query, text) {
+    if(text.includes(query)) {
+        return [query.length, query.length, text.indexOf(query)]
+    }
+    return [0, 0, 0]
 }
