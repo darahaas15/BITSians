4bcbd85d2d4a204f2e9879582b12fb1798b59150
robo-mop <realaryanpingle@gmail.com>
2022-03-26 22:51:18 +0530

    Rectified res cache issues

--- sw.js
+++ sw.js
@@ -5,14 +5,14 @@
 //       Suck it WorkboxJS.      //
 ///////////////////////////////////
 
-const APP_VERSION = 4.65
+const APP_VERSION = 4.66
 
 // Document Cache is a cache of document files - html, js, css, etc
 const DOCUMENT_CACHE_NAME = `DOC`
 var DOCUMENT_CACHE = null
 var DOCUMENT_FETCH_TYPE = null // For the user to select
 // Resource Cache is a cache of almost always static resources - images, fonts, and everything in the Texts folder
-const RESOURCE_VERSION = 4.60
+const RESOURCE_VERSION = 4.66
 const RESOURCE_CACHE_NAME = `RESv${RESOURCE_VERSION.toFixed(2)}`
 var RESOURCE_CACHE = null
 
