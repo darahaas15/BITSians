17e5b553eca89df8f3b8894e535fe3d9444505b3
robo-mop <realaryanpingle@gmail.com>
2022-04-04 14:44:06 +0530

    Dhruv Rohira 206

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const APP_VERSION = 5.00
 const DOCUMENT_CACHE_NAME = `DOC`
 var DOCUMENT_CACHE = null
 // Resource Cache is a cache of almost always static resources - images, fonts, and everything in the Texts folder
-const RESOURCE_VERSION = 5.00
+const RESOURCE_VERSION = 5.01
 const RESOURCE_CACHE_NAME = `RESv${RESOURCE_VERSION.toFixed(2)}`
 var RESOURCE_CACHE = null
 
